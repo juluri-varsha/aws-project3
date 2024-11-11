d25f92b0685c2b2f5d204774c93b144a5266adb3
juluri_varsha <juluri_varsha@epam.com>
2024-11-11 16:19:16 +0530

    changes are done

---
+++ task05/jsrc/main/java/com/task05/Response.java
@@ -0,0 +1,12 @@
+package com.task05;
+import lombok.*;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    private int statusCode;
+    private Event event;
+}
